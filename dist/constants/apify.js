4a993191362a97ea5d187b9557cf1a7406b8ad11
Saurabh Suryavanshi <ssuryavanshi@ineuron.ai>
2023-12-18 12:51:02 +0530

    removed dist from .git and fixed error

--- dist/constants/apify.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apifyPerUsernameResultLimit = void 0;
-exports.apifyPerUsernameResultLimit = 30;
--- dist/constants/dbquery.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.limit = void 0;
-exports.limit = 10;
